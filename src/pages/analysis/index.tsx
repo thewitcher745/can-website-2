2dd657e64f8ddddef9b429c51cdee2657db8f3a1
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-06-08 14:58:29 +0330

    FEAT: Added thumbnail linking to  the analysis posts

--- src/pages/analysis/index.tsx
+++ src/pages/analysis/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
 interface AnalysisPostMeta {
+  thumbnail_link: string;
   author: string;
   time: string;
   slug: string;
@@ -80,7 +82,7 @@ const Analysis: React.FC = () => {
     <>
       <Navbar />
       <main className="bg-background min-h-screen">
-        <div className="max-w-4xl mx-auto py-8 px-4 pt-24">
+        <div className="max-w-6xl mx-auto py-8 px-4 pt-24">
           <h1 className="text-3xl font-bold mb-8 text-primary">Analysis</h1>
           <p className="text-text-main text-xl mb-6 px-2">
             You can find our reliable, accurate and profitable premium analysis
@@ -93,34 +95,41 @@ const Analysis: React.FC = () => {
               filteredPosts.map((post) => (
                 <div
                   key={post.slug}
-                  className="bg-surface border border-border rounded-lg p-6 shadow transition hover:shadow-lg"
+                  className="bg-surface border border-border rounded-lg p-6 shadow transition hover:shadow-lg flex flex-col sm:flex-row gap-4 sm:gap-6 items-start sm:items-center"
                 >
-                  <h2 className="text-2xl font-semibold mb-2 text-text-main hover:text-primary transition-colors">
-                    <Link href={`/analysis/${post.slug}`}>{post.title}</Link>
-                  </h2>
-                  <p className="text-text-muted mb-3 line-clamp-3">
-                    {post.desc}
-                  </p>
-                  <div className="text-xs text-text-muted mb-3">
-                    Last updated at{" "}
-                    {new Date(post.time).toLocaleDateString(undefined, {
-                      year: "numeric",
-                      month: "2-digit",
-                      day: "2-digit",
-                    })}{" "}
-                    {new Date(post.time).toLocaleTimeString(undefined, {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                      hour12: false,
-                    })}
+                  <div className="flex-grow">
+                    <div className="flex gap-2 mb-4">
+                      {post?.thumbnail_link?.length > 0 && (
+                        <div className="flex-shrink-0 w-30 h-30 mr-2 lg:w-40 lg:h-40 sm:w-30 sm:h-30 relative">
+                          <Image
+                            src={post.thumbnail_link}
+                            alt={`${post.title} logo`}
+                            fill
+                            className="object-contain backdrop-blur-sm rounded-xl sm:p-2 py-2"
+                          />
+                        </div>
+                      )}
+                      <h2 className="text-2xl lg:text-4xl sm:text-3xl font-semibold mb-2 text-text-main hover:text-primary transition-colors">
+                        <Link href={`/analysis/${post.slug}`}>{post.title}</Link>
+                      </h2>
+                    </div>
+                    <p className="text-text-muted lg:text-xl px-2 mb-3 line-clamp-3">
+                      {post.desc}
+                    </p>
+                    <div className="text-xs text-text-muted px-2 mb-3">
+                      {post.time}
+                    </div>
+                    <div className="flex flex-wrap px-2 gap-2">
+                      {post.tags.map((tag) => (
+                        <span
+                          key={tag}
+                          className="px-3 py-1 bg-primary/10 text-primary text-sm rounded-full"
+                        >
+                          {tag}
+                        </span>
+                      ))}
+                    </div>
                   </div>
-
-                  <Link
-                    href={`/analysis/${post.slug}`}
-                    className="text-sm text-primary underline hover:text-primary-soft transition"
-                  >
-                    Read more →
-                  </Link>
                 </div>
               ))
             )}
