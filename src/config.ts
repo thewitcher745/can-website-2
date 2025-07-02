fcbc44959f34ce74a2852a8e903d540c3db33bf6
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-07-02 21:57:20 +0330

    FIX: Fixed API_URL setting

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-export const API_URL = process.env.API_URL || 'http://127.0.0.1:5000';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:5000';
+
 
 export const buildApiUrl = (path: string) => {
   return `${API_URL}${path.startsWith('/') ? '' : '/'}${path}`;
