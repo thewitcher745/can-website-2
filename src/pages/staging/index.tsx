3dbe11efb273e4ee4f551de33a62db98d23ec6cb
Mr.None <thewitcher745@gmail.com>
2025-08-05 12:50:04 +0330

    FEAT: Added staging page
    - This page would contain all the elements we are trying to add to the website

---
+++ src/pages/staging/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+import Navbar from "../../components/Navbar";
+import HeroSection from "../../components/HeroSection";
+import Footer from "../../components/Footer";
+import FearAndGreed from '../../components/technicals/FearAndGreed';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CAN Trading - Staging</title>
+      </Head>
+      <Navbar />
+      <main>
+        <HeroSection />
+      </main>
+      <Footer />
+    </>
+  );
+}
