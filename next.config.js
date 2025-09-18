7021765ff7aa97ccf1af4423256ff8f122014891
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-09-18 17:27:20 +0330

    FEAT: Updated allowed image links

--- next.config.js
+++ next.config.js
@@ -2,11 +2,36 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      new URL("https://alternative.me/**"),
-      new URL("http://static.photos/**"),
-      new URL("https://s2.coinmarketcap.com/**"),
-      new URL("https://img.freepik.com/**"),
-      new URL("https://www.tradingview.com/**"),
+      {
+        protocol: "https",
+        hostname: "alternative.me",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "static.photos",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s2.coinmarketcap.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.tradingview.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+        pathname: "/**",
+      },
     ],
   },
 };
