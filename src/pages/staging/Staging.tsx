ceaf6b83e0cb3277116dddeb67b5e565b0a12798
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-09-18 12:19:55 +0330

    FEAT: Added VideosSection to staging

--- src/pages/staging/Staging.tsx
+++ src/pages/staging/Staging.tsx
@@ -2,12 +2,14 @@ import OurStats from "../../components/homepage/promotions/ourStats/OurStats";
 import Banner from "../../components/homepage/promotions/Banner";
 import TopRowGadgetsSection from "../../components/TopRowGadgetsSection";
 import MostRecentAnalysisSection from "../../components/MostRecentAnalysis";
+import VideosSection from "../../components/VideosSection";
 
 const Staging: React.FC = () => {
   return (
     <main className="py-20 px-4 w-full bg-background flex flex-col items-center min-h-screen">
       <TopRowGadgetsSection />
       <MostRecentAnalysisSection />
+      <VideosSection />
       <OurStats />
       <div className="w-full pt-20 sm:pt-0 flex justify-center">
         <Banner />
