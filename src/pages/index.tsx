ebcf2f008ae77d4282a1f7597dd6bba26d2a6eec
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-09-29 21:57:25 +0330

    FEAT: Moved staging page to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import Head from "next/head";
 
 import Navbar from "../components/navbar/Navbar";
-import HeroSection from "../components/HeroSection";
-import ServicesSection from "../components/ServicesSection";
-import ResultsSection from "../components/ResultsSection";
-import PricingSection from "../components/PricingSection";
 import Footer from "../components/Footer";
+import Staging from "./index/Staging";
 
 export default function Home() {
   return (
@@ -14,13 +11,7 @@ export default function Home() {
         <title>CAN Trading - Home</title>
       </Head>
       <Navbar />
-      <main>
-        <HeroSection />
-        <ServicesSection />
-        <ResultsSection />
-        <PricingSection />
-        {/* Add other sections (Results, etc.) here as you modularize */}
-      </main>
+      <Staging />
       <Footer />
     </>
   );
