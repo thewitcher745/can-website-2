daf4e9cc63629cf544e6ad4f1ecf6b1bc93a15d2
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-10-02 21:24:22 +0330

    FEAT: Restyled MarketOverview/Stats element - Also added loaders and Error elements

--- src/features/coins/overview/TopVolumeCoinsChart/Stats.tsx
+++ src/features/coins/overview/TopVolumeCoinsChart/Stats.tsx
@@ -1,21 +1,75 @@
-import { useEffect, useState } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { MdCurrencyExchange, MdCurrencyBitcoin } from "react-icons/md";
+import { FaCoins } from "react-icons/fa6";
+import { FaDollarSign } from "react-icons/fa";
 
 import { buildApiUrl } from "@src/config";
 import { reduceNumber } from "@src/utils";
 import { MarketData } from "@src/types";
+import Caret from "@shared/ui/Caret";
+import { GenericLoader } from "@src/shared/ui/loaders";
+import GenericError from "@src/shared/ui/GenericError";
 
-const renderCaret = (change: number) => {
-  const isPositive = change >= 0;
-  const colorClass = isPositive ? "text-success" : "text-error";
+const LabelColumn = ({
+  label,
+  icon,
+}: {
+  label: string;
+  icon: React.JSX.Element;
+}) => {
   return (
-    <div className={`h-6 w-6 ${colorClass}`}>
-      {isPositive ? (
-        <ChevronUp className="h-6 w-6" />
+    <td className="py-6 w-1/2">
+      <div className="flex items-center text-text-muted gap-2">
+        {icon}
+        <span className="text-lg">{label}</span>
+      </div>
+    </td>
+  );
+};
+
+const StatRow = ({
+  label,
+  icon,
+  value,
+  change,
+}: {
+  label: string;
+  icon: React.JSX.Element;
+  value: number;
+  change?: number;
+}) => {
+  const labelValue =
+    label === "Market Cap" || label === "Total Volume"
+      ? reduceNumber(value)
+      : value;
+
+  return (
+    <tr className="table w-full xl:w-3/4">
+      <LabelColumn label={label} icon={icon} />
+      <td className="w-1/4">
+        <div className="flex xs:flex-row flex-col gap-2 items-center justify-end">
+          <span className="text-text-main text-right text-xl font-semibold">
+            {labelValue}
+          </span>
+        </div>
+      </td>
+      {change ? (
+        <td className="w-1/4">
+          <div
+            className={`pl-2 text-text-black size-full flex items-center text-lg font-semibold ${
+              change >= 0 ? "text-success" : "text-error"
+            }`}
+          >
+            <Caret change={change} />{" "}
+            <span>{Math.abs(change).toFixed(2)}%</span>
+          </div>
+        </td>
       ) : (
-        <ChevronDown className="h-6 w-6" />
+        <td className="w-1/4">
+          <div className="pl-2 text-text-black size-full flex items-center text-lg font-semibold"></div>
+        </td>
       )}
-    </div>
+    </tr>
   );
 };
 
@@ -34,84 +88,64 @@ const Stats = () => {
   useEffect(() => {
     fetch(buildApiUrl(`/api/market_data`))
       .then((res) => {
-        if (!res.ok) throw new Error("Failed to fetch top coins.");
+        if (!res.ok) throw new Error("Failed to fetch market data.");
         return res.json();
       })
       .then((response) => {
         setMarketData(response);
       })
       .catch((e) => setError(e.message))
-      .finally(() => setLoading(false));
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      });
   }, []);
 
+  const LoaderElement = () => {
+    return (
+      <div className="pl-2 w-full sm:w-1/2 min-h-75 lg:w-full flex justify-center items-center">
+        <GenericLoader />
+      </div>
+    );
+  };
+
+  const ErrorElement = () => {
+    return (
+      <div className="pl-2 w-full sm:w-1/2 min-h-75 lg:w-full flex justify-center items-center">
+        <GenericError message={error || "Failed to fetch market data."} />
+      </div>
+    );
+  };
+
+  if (loading) return <LoaderElement />;
+  if (error) return <ErrorElement />;
+
   return (
-    <table className="pl-2 w-full flex flex-col gap-4 rounded-lg">
-      <tbody>
-        <tr>
-          <td className="text-text-muted text-xl text-right p-2 py-6 w-1/2">
-            Market Cap
-          </td>
-          <td>
-            <div className="flex xs:flex-row flex-col items-center justify-between">
-              <span className="text-text-main text-2xl font-semibold">
-                {reduceNumber(marketData.totalMarketCap)}
-              </span>
-              <div>
-                <div
-                  className={`text-text-black flex items-center text-xl font-semibold ${
-                    marketData.marketCapChangePercentage24h >= 0
-                      ? "text-success"
-                      : "text-error"
-                  }`}
-                >
-                  {renderCaret(marketData.marketCapChangePercentage24h)}{" "}
-                  {Math.abs(marketData.marketCapChangePercentage24h).toFixed(2)}
-                  %
-                </div>
-              </div>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td className="text-text-muted text-xl w-1/2 text-right p-2 py-6">
-            24h Volume
-          </td>
-          <td>
-            <div className="flex xs:flex-row flex-col items-center justify-between">
-              <span className="text-text-main text-2xl font-semibold">
-                {reduceNumber(marketData.totalVolume24h)}
-              </span>
-              <div>
-                <div
-                  className={`text-text-black flex items-center text-xl font-semibold ${
-                    marketData.volumeChangePercentage24h >= 0
-                      ? "text-success"
-                      : "text-error"
-                  }`}
-                >
-                  {renderCaret(marketData.volumeChangePercentage24h)}{" "}
-                  {Math.abs(marketData.volumeChangePercentage24h).toFixed(2)}%
-                </div>
-              </div>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td className="text-text-muted text-xl w-1/2 text-right p-2 py-6">
-            Active exchanges
-          </td>
-          <td className="text-text-main text-2xl font-semibold p-2 py-6">
-            {marketData.activeExchanges}
-          </td>
-        </tr>
-        <tr>
-          <td className="text-text-muted text-xl w-1/2 text-right p-2 py-6">
-            Active cryptocurrencies
-          </td>
-          <td className="text-text-main text-2xl font-semibold p-2 py-6">
-            {marketData.activeCurrencies}
-          </td>
-        </tr>
+    <table className="pl-2 w-full sm:w-1/2 min-w-75 lg:w-full table gap-4 rounded-lg">
+      <tbody className="w-full">
+        <StatRow
+          label="Market Cap"
+          icon={<FaCoins className="w-6 h-6" />}
+          value={marketData.totalMarketCap}
+          change={marketData.marketCapChangePercentage24h}
+        />
+        <StatRow
+          label="Total Volume"
+          icon={<FaDollarSign className="w-6 h-6" />}
+          value={marketData.totalVolume24h}
+          change={marketData.volumeChangePercentage24h}
+        />
+        <StatRow
+          label="Active Currencies"
+          icon={<MdCurrencyBitcoin className="w-6 h-6" />}
+          value={marketData.activeCurrencies}
+        />
+        <StatRow
+          label="Active Exchanges"
+          icon={<MdCurrencyExchange className="w-6 h-6" />}
+          value={marketData.activeExchanges}
+        />
       </tbody>
     </table>
   );
