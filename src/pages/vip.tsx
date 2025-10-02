cfc99bab20d721d57e82c0b9b17fc9ec24f682b1
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-10-02 20:03:36 +0330

    FEAT: Added old homepage as /vip

---
+++ src/pages/vip.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+import Navbar from "@shared/ui/navbar/Navbar";
+import HeroSection from "@features/homepage/sections/HeroSection";
+import ServicesSection from "@features/homepage/sections/ServicesSection";
+import ResultsSection from "@features/homepage/sections/ResultsSection";
+import PricingSection from "@features/homepage/sections/PricingSection";
+import Footer from "@shared/ui/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CAN Trading - Home</title>
+      </Head>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <ServicesSection />
+        <ResultsSection />
+        <PricingSection />
+      </main>
+      <Footer />
+    </>
+  );
+}
