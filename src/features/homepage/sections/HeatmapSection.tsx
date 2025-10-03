9ce62b6c17287bfd64d8292673c10574941c5b0f
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-10-03 13:58:26 +0330

    FIX: Fixed crypto heatmap title href

--- src/features/homepage/sections/HeatmapSection.tsx
+++ src/features/homepage/sections/HeatmapSection.tsx
@@ -8,7 +8,7 @@ const HeatmapSection: React.FC = () => {
     <section id="heatmap" className="w-full flex justify-center my-2">
       <div className="2xl:max-w-[100rem] xl:max-w-7xl max-w-6xl w-full flex flex-col items-center lg:items-start justify-center">
         <Link
-          href="/heatmap"
+          href="/coins/overview"
           className="text-text-main font-bold transition-all duration-200 flex items-center pb-4"
         >
           <h2 className="text-2xl font-bold text-text-main title-hover underline">
