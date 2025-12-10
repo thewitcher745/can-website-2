eb24a180c26da2d80475e6109afded1092d3334a
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-12-10 14:14:16 +0330

    FEAT: Updated Google tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import "../styles/globals.css";
 import "../styles/loader.css";
 import "../styles/articles.css";
@@ -11,22 +13,25 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <Script id="gtag-config" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NZJH59BG');
+        `}
+        </Script>
         <link rel="icon" href="/favicon.png" type="image/png" sizes="any" />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17663344120"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-17663344120');
-            `,
-          }}
-        />
       </Head>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NZJH59BG"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
       <Navbar />
       <Component {...pageProps} />
     </>
