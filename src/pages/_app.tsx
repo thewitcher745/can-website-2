699f590796ab9eb8db608e4b6c8d4a976e761325
Mohammad Amin Seifinia <53443483+thewitcher745@users.noreply.github.com>
2026-01-30 16:11:41 +0330

    FEAT: Added X Pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,11 +8,25 @@ import "../styles/results.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Navbar from "@shared/ui/navbar/Navbar";
+import { useTwitterPageView } from "@src/shared/useTwitterPageView";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useTwitterPageView();
+
   return (
     <>
-      <Script id="gtag-config" strategy="beforeInteractive">
+      {/* X Pixel */}
+      <Script id="twitter-pixel" strategy="afterInteractive">
+        {`
+          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','ou3hs');
+        `}
+      </Script>
+
+      {/* GTag Script */}
+      <Script id="gtag-config" strategy="afterInteractive">
         {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -21,9 +35,13 @@ function MyApp({ Component, pageProps }: AppProps) {
           })(window,document,'script','dataLayer','GTM-NZJH59BG');
         `}
       </Script>
+
+      {/* Favicon */}
       <Head>
         <link rel="icon" href="/favicon.png" type="image/png" sizes="any" />
       </Head>
+
+      {/* GTag Noscript */}
       <noscript>
         <iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-NZJH59BG"
@@ -32,6 +50,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           style={{ display: "none", visibility: "hidden" }}
         ></iframe>
       </noscript>
+
       <Navbar />
       <Component {...pageProps} />
     </>
