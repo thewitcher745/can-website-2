ff0e725032bf0b149d405b20c39beb561d6d4a13
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2026-05-24 11:49:06 +0000

    FIX: Fixed API URL being set to the dev URL

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,5 +1,5 @@
-export const API_URL = "http://127.0.0.1:5000";
-
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+console.log(process.env);
 export class ApiError extends Error {
   constructor(
     message: string,
